ec3d5c2d5b82c1547ee4a782b6053f653cbb3fb0
WOOD22 <90883790+WOOD22@users.noreply.github.com>
2021-10-18 18:14:05 +0900

    무한 스크롤 작업

--- Assets/Script/Ui/Infinite_Scroll.cs
+++ Assets/Script/Ui/Infinite_Scroll.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 //스크롤 뷰의 Content에 달아서 사용
 public class Infinite_Scroll : MonoBehaviour
@@ -9,37 +8,41 @@ public class Infinite_Scroll : MonoBehaviour
     //선언=========================================================================================
     public GameObject scroll_cell_num0;//0번 셀
     public bool is_pooling;//풀링 유무
-    public float content_x, right_x;//Content의 x좌표, Content의 순환 크기
+    public bool is_dial;
+    public float content_x;//Content의 x좌표
     private float view_width, cell_width;//Viewport의 넓이, Cell의 넓이
 
     void Start()
     {
         //초기화===================================================================================
-        right_x = (-1 * this.GetComponent<RectTransform>().rect.width) + scroll_cell_num0.GetComponent<RectTransform>().rect.width;
         view_width = this.transform.parent.GetComponent<RectTransform>().rect.width;
         cell_width = scroll_cell_num0.GetComponent<RectTransform>().rect.width;
     }
-
     void Update()
     {
         //매 프레임 초기화=========================================================================
-        content_x = this.GetComponent<RectTransform>().localPosition.x;
-        //좌측 끝까지 이동시 우측 끝으로 이동======================================================
-        if (content_x > 0)
-        {
-            this.GetComponent<RectTransform>().localPosition = new Vector2(right_x + content_x, 0);
-        }
-        //우측 끝까지 이동시 좌측 끝으로 이동======================================================
-        else if (content_x <= right_x)
-        {
-            this.GetComponent<RectTransform>().localPosition = new Vector2(content_x - right_x, 0);
-        }
+        content_x = -1 * this.GetComponent<RectTransform>().localPosition.x;
+        loop();
         //풀링 ture================================================================================
-        if(is_pooling == true)
+        if (is_pooling == true)
         {
             pooling();
         }
+        if(Input.GetMouseButtonDown(0))
+        {
+
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Dial();
+        }
     }
+
+    void Dial()
+    {
+        this.GetComponent<RectTransform>().localPosition = new Vector2(-1 * (Mathf.Floor(content_x / 100) * 100 + cell_width / 2), 0);
+    }
+
     //풀링 함수(순환 시에 Content를 순환 방향으로 드래그 할 경우 좌표가 튀는 현상 발생)============
     void pooling()
     {
@@ -47,8 +50,8 @@ public class Infinite_Scroll : MonoBehaviour
         for (int i = 0; i < this.transform.childCount; i++)
         {
             //Viewport 안에 있는 Cell만 활성화=====================================================
-            if ((-1 * content_x) - cell_width * (i) < this.transform.GetChild(i).GetComponent<RectTransform>().position.x &&
-                (-1 * content_x) + view_width - cell_width * (i) > this.transform.GetChild(i).GetComponent<RectTransform>().position.x)
+            if (content_x - cell_width < this.transform.GetChild(i).GetComponent<RectTransform>().localPosition.x &&
+                content_x + view_width > this.transform.GetChild(i).GetComponent<RectTransform>().localPosition.x)
             {
                 this.transform.GetChild(i).gameObject.SetActive(true);
             }
@@ -57,6 +60,21 @@ public class Infinite_Scroll : MonoBehaviour
             {
                 this.transform.GetChild(i).gameObject.SetActive(false);
             }
+        }
+    }
+    void loop()
+    {
+        //좌측으로 이동============================================================================
+        if (content_x < this.transform.GetChild(0).GetComponent<RectTransform>().localPosition.x)
+        {
+            this.transform.GetChild(this.transform.childCount - 1).GetComponent<RectTransform>().localPosition = new Vector2(this.transform.GetChild(0).GetComponent<RectTransform>().localPosition.x - cell_width, 0);
+            this.transform.GetChild(this.transform.childCount - 1).SetSiblingIndex(0);
+        }
+        //우측으로 이동============================================================================
+        else if (content_x + cell_width > (this.transform.GetChild(this.transform.childCount - 1).GetComponent<RectTransform>().localPosition.x)) 
+        {
+            this.transform.GetChild(0).GetComponent<RectTransform>().localPosition = new Vector2(this.transform.GetChild(this.transform.childCount - 1).GetComponent<RectTransform>().localPosition.x + cell_width, 0);
+            this.transform.GetChild(0).SetSiblingIndex(this.transform.childCount - 1);
         }
     }
 }
