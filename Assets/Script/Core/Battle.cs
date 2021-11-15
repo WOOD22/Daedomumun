1774acad58f665f4b028ab37c5accf2e63a249ab
WOOD22 <90883790+WOOD22@users.noreply.github.com>
2021-11-15 15:57:37 +0900

    2021.11.15

--- Assets/Script/Core/Battle.cs
+++ Assets/Script/Core/Battle.cs
@@ -9,58 +9,82 @@ public class Battle : MonoBehaviour
     //blue : 청코너
     public BattleStat red;
     public BattleStat blue;
-    //전투 시작시 체력 초기화======================================================================
+    //초기화 용도==================================================================================
+    void Initialization(BattleStat user)
+    {
+        user.max_HP = (int)Mathf.Floor(user.student.st_CON) * 20;
+        user.now_HP = user.max_HP;
+        user.max_SP = int.Parse(user.student.active_skill.need);
+        user.now_SP = 0;
+        user.ATK_cool = 2.5f - user.student.st_DEX / 100;
+        user.ATK_can = true;
+        user.SP_cool = 2.5f - user.student.st_INT / 100;
+        user.SP_can = true;
+    }
+    //전투 시작시 초기화===========================================================================
     void Start()
     {
-        red.max_HP = (int)Mathf.Floor(red.student.st_CON) * 100;
-        red.now_HP = red.max_HP;
-        red.max_SP = int.Parse(red.student.active_skill.need);
-        red.now_SP = 0;
-        red.cool_ATK = 2.5f - red.student.st_DEX / 100;
-        red.can_ATK = true;
-
-        blue.max_HP = (int)Mathf.Floor(red.student.st_CON) * 100;
-        blue.now_HP = red.max_HP;
-        blue.max_SP = int.Parse(blue.student.active_skill.need);
-        blue.now_SP = 0;
-        blue.cool_ATK = 2.5f - blue.student.st_DEX / 100;
-        blue.can_ATK = true;
+        Initialization(red);
+        Initialization(blue);
+    }
+    //일반 공격====================================================================================
+    void ATK_Can(BattleStat user, BattleStat target)
+    {
+        if (user.ATK_can == true)
+        {
+            red.ATK_can = false;
+            StartCoroutine(ATK_Cooltime(user));
+            ATK_Target(user, target);
+        }
+    }
+    //액티브 스킬==================================================================================
+    void SP_Can(BattleStat user, BattleStat target)
+    {
+        if (user.SP_can == true)
+        {
+            red.SP_can = false;
+            StartCoroutine(SP_Cooltime(user));
+            Charge_SP(user);
+            if (user.now_SP >= user.max_SP)
+            {
+                Use_Active_Skill(user.student.active_skill.effect, user, target);
+            }
+        }
     }
 
     void Update()
     {
-        if(red.can_ATK == true)
-        {
-            red.can_ATK = false;
-            StartCoroutine(ATK_Cooltime(red));
-            Target_ATK(red, blue);
-        }
+        ATK_Can(red, blue);
+        ATK_Can(blue, red);
 
-        if (blue.can_ATK == true)
-        {
-            blue.can_ATK = false;
-            StartCoroutine(ATK_Cooltime(blue));
-            Target_ATK(blue, red);
-        }
+        SP_Can(red, blue);
+        SP_Can(blue, red);
     }
-
-    void Target_ATK(BattleStat user, BattleStat target)
+    //일반 공격(데미지는 STR에 비례, LUK으로 추가 데미지 배율 랜덤 적용)===========================
+    void ATK_Target(BattleStat user, BattleStat target)
     {
         int target_DMG = (-1) * (int)(user.student.st_STR * (1 + Random.Range(0, user.student.st_LUK) / 200));
         target.now_HP += target_DMG;
         Debug.Log(target.student.name + target_DMG);
-        user.can_ATK = false;
+        user.ATK_can = false;
     }
-
+    //일반 공격 속도(0 = 2.5 초당 1회, 100 = 1.5 초당 1회, 200 = 0.5 초당 1회)=================================
     IEnumerator ATK_Cooltime(BattleStat user)
     {
-        yield return new WaitForSeconds(user.cool_ATK);
-        user.can_ATK = true;
+        yield return new WaitForSeconds(user.ATK_cool);
+        user.ATK_can = true;
     }
-
-    void Charge_SP(float time, BattleStat user)
+    //SP 충전======================================================================================
+    void Charge_SP(BattleStat user)
     {
-        user.now_SP += (int)(red.student.st_INT / 10);
+        user.now_SP += (int)(red.student.st_WIS / 10);
+        user.SP_can = false;
+    }
+    //SP 충전 속도(0 = 초당)
+    IEnumerator SP_Cooltime(BattleStat user)
+    {
+        yield return new WaitForSeconds(user.SP_cool);
+        user.SP_can = true;
     }
     //액티브 스킬 발동(복수의 effect의 경우 &를 기준으로 나누어서 적용 ## & 는 문자열에서 제외한다)
     public void Use_Active_Skill(string skill_effect, BattleStat user, BattleStat target)
@@ -100,7 +124,6 @@ public class Battle : MonoBehaviour
                 {
                     effect_hit_num = skill_effect.IndexOf(",", effect_facter + 1);
                 }
-
                 //stat    : 계수가 적용되는 스탯
                 //facter  : 적용되는 계수
                 //hit_num : 타격 횟수
@@ -108,7 +131,8 @@ public class Battle : MonoBehaviour
                 string facter = skill_effect.Substring(effect_stat + 1, effect_facter - 1 - effect_stat);
                 string hit_num = skill_effect.Substring(effect_facter + 1, effect_hit_num - 1 - effect_facter);
 
-                Active_DMG(stat, facter, hit_num, user, target);
+                Active_DMG(stat, facter, hit_num, user, target);//DMG효과 발동
+                user.now_SP = 0;
                 break;
         }
     }
@@ -120,15 +144,15 @@ public class Battle : MonoBehaviour
             case "STR":
                 for (int i = 0; i < int.Parse(hit_num); i++)
                 {
-                    target.now_HP += (int)((-1) * Mathf.Floor(user.student.st_STR) * int.Parse(facter));
-                    Debug.Log((int)((-1) * Mathf.Floor(user.student.st_DEX) * int.Parse(facter)));
+                    target.now_HP += (int)((-1) * Mathf.Floor(user.student.st_STR) * float.Parse(facter));
+                    Debug.Log((int)((-1) * Mathf.Floor(user.student.st_DEX) * float.Parse(facter)));
                 }
                 break;
             case "DEX":
                 for (int i = 0; i < int.Parse(hit_num); i++)
                 {
-                    target.now_HP += (int)((-1) * Mathf.Floor(user.student.st_DEX) * int.Parse(facter));
-                    Debug.Log((int)((-1) * Mathf.Floor(user.student.st_DEX) * int.Parse(facter)));
+                    target.now_HP += (int)((-1) * Mathf.Floor(user.student.st_DEX) * float.Parse(facter));
+                    Debug.Log((int)((-1) * Mathf.Floor(user.student.st_DEX) * float.Parse(facter)));
                 }
                 break;
         }
@@ -138,11 +162,13 @@ public class Battle : MonoBehaviour
 [System.Serializable]
 public class BattleStat
 {
-    public int max_HP;
-    public int now_HP;
-    public int max_SP;
-    public int now_SP;
-    public Student student;
-    public bool can_ATK;
-    public float cool_ATK;
+    public int max_HP;      //최대 체력
+    public int now_HP;      //현재 체력
+    public int max_SP;      //최대 SP
+    public int now_SP;      //현재 SP
+    public bool ATK_can;    //공격 가능?
+    public float ATK_cool;  //공격 속도
+    public bool SP_can;     //SP 충전 가능?
+    public float SP_cool;   //SP 충전 속도
+    public Student student; //파이터 데이터
 }
