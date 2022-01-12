046183a23656894ca305fe30fd72b4da6ad5adf2
WOOD22 <90883790+WOOD22@users.noreply.github.com>
2022-01-12 21:35:37 +0900

    2022.01.12

--- Assets/Script/Core/Training.cs
+++ Assets/Script/Core/Training.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Training : MonoBehaviour
 {
     public GameObject prefab_portrait_card;
     public GameObject content;
     public School player_school = new School();
+    public List<Student> player_student_list = new List<Student>();
 
     public void Init_Training_Unit()
     {
         player_school = GameObject.Find("GameData").GetComponent<Game_Data>().game_data.schools[1];
-
+        //문자순으로 정렬==========================================================================
+        player_student_list = player_school.students;
+        var temp = player_student_list.OrderBy(x => x.name);
+        player_student_list = temp.ToList<Student>();
+        //===========
         for (int i = 0; i < player_school.students.Count; i++)
         {
             GameObject instance;
             instance = Instantiate(prefab_portrait_card, content.transform);
-            instance.GetComponent<Prefab_Portrait_Card_Property>().student = player_school.students[i];
+            instance.GetComponent<Prefab_Portrait_Card_Property>().student = player_student_list[i];
         }
     }
 }
