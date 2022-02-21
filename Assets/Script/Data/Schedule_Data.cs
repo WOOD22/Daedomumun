5e8a96e9a6f2a7a6bc622502dad31484b1bfdd0f
WOOD22 <90883790+WOOD22@users.noreply.github.com>
2022-02-21 22:52:00 +0900

    2022.02.21

--- Assets/Script/Data/Schedule_Data.cs
+++ Assets/Script/Data/Schedule_Data.cs
@@ -12,6 +12,22 @@ public class Schedule_Data : MonoBehaviour
     void Start()
     {
         game_data = GameObject.Find("GameData").GetComponent<Game_Data>();
+        schedule_table = CSVReader.Read("DataBase/CSV/Schedule_Table");
+        for (int i = 0; i < schedule_table.Count; i++)
+        {
+            Schedule new_schedule = new Schedule();
+
+            new_schedule.name = schedule_table[i]["name"].ToString();
+            new_schedule.type = schedule_table[i]["type"].ToString();
+            new_schedule.ticket = schedule_table[i]["ticket"].ToString();
+            new_schedule.month = int.Parse(schedule_table[i]["month"].ToString());
+            new_schedule.prestige = int.Parse(schedule_table[i]["prestige"].ToString());
+            new_schedule.money = int.Parse(schedule_table[i]["money"].ToString());
+            if(new_schedule.month >= 10)
+            {
+                new_schedule.code = "S" + new_schedule.month.ToString() + Random.Range(0, 10).ToString() + Random.Range(0, 10).ToString();
+            }
+        }
     }
 
     public void Set_Schedule()
